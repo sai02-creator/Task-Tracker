13c20254c94e2678c03cb7ca516de791783f5f57
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-05 23:18:57 +1000

    Add ul for todoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,16 @@
 const todoList = ["Do Homework", "Clean Room", "Take Out Chicken"];
+
 function TodoList() {
-    return <div> Todo List </div>
+    return <div>
+              <h2> Todo List </h2>
+
+              <ul>
+                {todoList.map((task, key) => (
+                    <li key={key}>{task}</li>
+                ))}
+              </ul>
+              
+          </div>
 }
 
 export default TodoList;
