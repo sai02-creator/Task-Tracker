a6894307dc9f090f2f47d11d4cbebecb29129e53
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-08 00:59:09 +1000

    Add iscompleted : false in addTask function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -11,7 +11,7 @@ function TodoList() {
 
     function addTask() {
       const lastId = todoList[todoList.length - 1].id;
-      setTodoList((prev) => [...prev, {id: lastId + 1, taskName: newTask}]);
+      setTodoList((prev) => [...prev, {id: lastId + 1, taskName: newTask, isCompleted: false}]);
       setNewTask("");
        
 
