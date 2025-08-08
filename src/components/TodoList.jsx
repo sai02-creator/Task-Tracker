4125a820240f71894c106b78da76472a0ad82b75
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-09 00:11:59 +1000

    Turn whole logic in to turneary operator in function completeTask

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,64 +1,61 @@
 import { useState } from "react";
 
-
 function TodoList() {
-    const [newTask, setNewTask] = useState("");
-    const [todoList, setTodoList] = useState([
-      { id:1, taskName: "Do Homework", isCompleted: false },
-      { id:2, taskName: "Clean Room", isCompleted: false},
-      { id:3, taskName: "Take Out Chicken", isCompleted: false},
+  const [newTask, setNewTask] = useState("");
+  const [todoList, setTodoList] = useState([
+    { id: 1, taskName: "Do Homework", isCompleted: false },
+    { id: 2, taskName: "Clean Room", isCompleted: false },
+    { id: 3, taskName: "Take Out Chicken", isCompleted: false },
+  ]);
+
+  function addTask() {
+    const lastId = todoList[todoList.length - 1].id;
+    setTodoList((prev) => [
+      ...prev,
+      { id: lastId + 1, taskName: newTask, isCompleted: false },
     ]);
+    setNewTask("");
+  }
 
-    function addTask() {
-      const lastId = todoList[todoList.length - 1].id;
-      setTodoList((prev) => [...prev, {id: lastId + 1, taskName: newTask, isCompleted: false}]);
-      setNewTask("");
-       
+  function deleteTask(id) {
+    setTodoList((prev) => prev.filter((task) => task.id !== id));
+  }
 
-    }
-
-
-    function deleteTask(id){
-     
-      setTodoList((prev) => prev.filter((task) => task.id !==id));
-
-
-    }
-    function completeTask(id){
-     
-      setTodoList((prev) => 
-        prev.map((task) => {
-
-          if (task.id === id) {
-            return { ...task, isCompleted: true };
-          } else {
-            return task;
-          }
-      } ));
-
-
-    }
-
-
-
-    return (
-          <div>
-              <h2> Todo List </h2>
-
-              <input type="text" placeholder="New task..." value={newTask} onChange={(event) => setNewTask(event.target.value)}/>
-              <button onClick={addTask} disabled={!newTask}> Add task </button>
-
-              <ul>
-                {todoList.map((task, key) => (
-                    <li key={key}>{task.taskName}
-                    <p>Task Completed: {task.isCompleted ? "Yes" : "No"} </p>
-                    <button onClick={() => completeTask(task.id)}> Complete Task </button>
-                    <button onClick={() => deleteTask(task.id)}> Delete </button></li>
-                ))}
-              </ul>
-              
-          </div>
+  function completeTask(id) {
+    setTodoList((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, isCompleted: true } : task
+      )
     );
+  }
+
+  return (
+    <div>
+      <h2>Todo List </h2>
+
+      <input
+        type="text"
+        placeholder="New task..."
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+      />
+      <button onClick={addTask} disabled={!newTask}>
+        Add Task
+      </button>
+
+      <ul>
+        {todoList.map((task, key) => (
+          <li key={key}>
+            {" "}
+            {task.taskName}
+            <p>Task Completed: {task.isCompleted ? "Yes" : "No"}</p>
+            <button onClick={() => completeTask(task.id)}>Complete Task</button>
+            <button onClick={() => deleteTask(task.id)}> Delete </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
